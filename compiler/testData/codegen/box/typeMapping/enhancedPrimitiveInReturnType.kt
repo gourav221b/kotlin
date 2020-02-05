2da491008dd27655687b49c4ef7a5580b64c9c6e
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2020-02-05 16:02:52 +0100

    JVM IR: keep original KotlinType in eraseTypeParameters
    Otherwise EnhancedNullability annotation instance is lost (for some
    reason it's not translated to IR elements), and in the newly added test,
    the wrapper type in Java is confused with primitive, and this causes
    a platform declaration clash error.
    
    Also make IrTypeArgument.eraseTypeParameters private, since it's not
    used outside.

---
+++ compiler/testData/codegen/box/typeMapping/enhancedPrimitiveInReturnType.kt
@@ -0,0 +1,17 @@
+// IGNORE_BACKEND_FIR: JVM_IR
+// TARGET_BACKEND: JVM
+
+// FILE: E.java
+
+public interface E {
+    @org.jetbrains.annotations.NotNull
+    Integer getId();
+}
+
+// FILE: box.kt
+
+class EImpl : E {
+    override fun getId(): Int = 314
+}
+
+fun box(): String = if (EImpl().getId() == 314) "OK" else "Fail"
